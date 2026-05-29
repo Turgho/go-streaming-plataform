defce7d34f05f00640ee55aa613adf0bd3695938
Turgho <victor.hugo3692111@gmail.com>
2026-05-28 22:33:23 -0300

    feat: add user repository interface

---
+++ services/user/internal/domain/repository/user_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+	"user-service/internal/domain/entities"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *entities.User) error
+	GetByID(ctx context.Context, id string) (*entities.User, error)
+	GetByEmail(ctx context.Context, email string) (*entities.User, error)
+}
