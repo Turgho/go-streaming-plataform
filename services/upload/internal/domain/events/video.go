ddcc1b06ace50ea7551f8c3f5ac0a2d4934abf47
Turgho <victor.hugo3692111@gmail.com>
2026-05-30 20:36:22 -0300

    refactor: remove video event

--- services/upload/internal/domain/events/video.go
+++
@@ -1,9 +0,0 @@
-package events
-
-const VideoUploaded = "video.uploaded"
-
-type VideoUploadedEvent struct {
-	VideoID  string `json:"video_id"`
-	FilePath string `json:"file_path"`
-	Mimetype string `json:"mimetype"`
-}
