f78ca90c63f8a0c649fbe08224b16e59de8b7bdc
Turgho <victor.hugo3692111@gmail.com>
2026-05-30 20:37:46 -0300

    refactor(interceptor): simplify AuthStreamInterceptor logic

--- services/upload/internal/infra/interceptor/stream.go
+++ services/upload/internal/infra/interceptor/stream.go
@@ -2,55 +2,41 @@ package interceptor
 
 import (
 	"context"
-	"log"
-	"strings"
+
 	userpb "upload-service/pkg/userpb"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 type contextKey string
 
 const UserIDKey contextKey = "user_id"
 
+// AuthStreamInterceptor autentica streams e adiciona o user_id ao contexto.
 func AuthStreamInterceptor(userClient userpb.UserServiceClient) grpc.StreamServerInterceptor {
-	return func(srv any, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		log.Println("StreamInterceptor chamado")
-
-		md, ok := metadata.FromIncomingContext(stream.Context())
-		log.Printf("metadata ok: %v", ok)
-		log.Printf("metadata: %v", md)
-		if !ok {
-			return status.Error(codes.Unauthenticated, "metadata ausente")
-		}
-
-		tokens := md.Get("authorization")
-		if len(tokens) == 0 {
-			return status.Error(codes.Unauthenticated, "token ausente")
-		}
-
-		token := strings.TrimPrefix(tokens[0], "Bearer ")
-		log.Printf("tokens: %v", tokens)
-
-		user, err := userClient.ValidateToken(context.Background(), &userpb.ValidateTokenRequest{Token: token})
+	return func(
+		srv any,
+		stream grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		userID, err := authenticateUser(stream.Context(), userClient)
 		if err != nil {
-			log.Printf("ValidateToken err: %v", err)
-			return status.Error(codes.Unauthenticated, "token inválido")
+			return err
 		}
 
-		ctx := context.WithValue(stream.Context(), UserIDKey, user.UserId)
-		return handler(srv, &wrappedStream{stream, ctx})
+		ctx := context.WithValue(stream.Context(), UserIDKey, userID)
+		return handler(srv, &wrappedStream{ServerStream: stream, ctx: ctx})
 	}
 }
 
+// wrappedStream substitui o contexto original do stream.
 type wrappedStream struct {
 	grpc.ServerStream
 	ctx context.Context
 }
 
+// Context retorna o contexto enriquecido.
 func (w *wrappedStream) Context() context.Context {
 	return w.ctx
 }
