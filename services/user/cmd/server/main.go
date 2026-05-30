e9d217ea7ba59c15361f3efd539a681a0342586f
Turgho <victor.hugo3692111@gmail.com>
2026-05-30 20:40:29 -0300

    feat: refactor user service to use usecase layer

--- services/user/cmd/server/main.go
+++ services/user/cmd/server/main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"net"
 	"os"
+
+	grpchandler "user-service/internal/handler/grpc"
 	"user-service/internal/infra/database"
 	"user-service/internal/repository"
-	"user-service/internal/server"
+	"user-service/internal/usecase"
 	"user-service/pkg/pb"
 
 	"google.golang.org/grpc"
@@ -19,22 +21,23 @@ func main() {
 
 	db, err := database.NewMongoDatabase(ctx, os.Getenv("MONGO_URI"), os.Getenv("MONGO_DB"))
 	if err != nil {
-		log.Fatalf("failed to init mongo database: %v", err)
+		log.Fatalf("mongo: %v", err)
 	}
 
-	userRepo := repository.NewUserRepository(db.Client)
+	userRepo := repository.NewUserRepository(db.Client, os.Getenv("MONGO_DB"))
+	userUC := usecase.NewUserUseCase(userRepo)
 
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("failed to init server: %v", err)
+		log.Fatalf("listen: %v", err)
 	}
-	grpcServer := grpc.NewServer()
-	pb.RegisterUserServiceServer(grpcServer, server.NewUserServer(userRepo))
 
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, grpchandler.NewServer(userUC))
 	reflection.Register(grpcServer)
 
-	log.Println("server running on port: 50051")
+	log.Println("user-service rodando na porta :50051")
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to run server: %v", err)
+		log.Fatalf("serve: %v", err)
 	}
 }
